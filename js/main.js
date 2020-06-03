6ba3eac3d1723419cfff316c9c323f0bb5ab9a5d
unknown <maksim.saraev.121.5@gmail.com>
2020-06-03 17:02:20 +0300

    fix: slider fix

--- js/main.js
+++ js/main.js
@@ -29,22 +29,22 @@ $(document).ready(function() {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
           },
-          // breakpoints: {
-          //   // when window width is >= 320px
-          //   320: {
-          //     slidesPerView: 1,
-          //     spaceBetween: 20
-          //   },
-          //   // when window width is >= 480px
-          //   480: {
-          //     slidesPerView: 1,
-          //     spaceBetween: 30
-          //   },
-          //   // when window width is >= 640px
-          //   640: {
-          //     slidesPerView: 1,
-          //     spaceBetween: 40
-          //   }
-          // },
+          breakpoints: {
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 20
+            },
+            // when window width is >= 480px
+            480: {
+              slidesPerView: 1,
+              spaceBetween: 30
+            },
+            // when window width is >= 640px
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 30
+            }
+          },
       });
 });
