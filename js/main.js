92050865fbbd9a07f75103ac3fcac8e952b5fff7
unknown <maksim.saraev.121.5@gmail.com>
2020-06-03 15:13:36 +0300

    feat:added slider

--- js/main.js
+++ js/main.js
@@ -17,4 +17,34 @@ $(document).ready(function() {
       function videoPlay(event){
         event.target.playVideo();
       }
+      //slider
+      var mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+            loop: true,
+            pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+          },
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+          // breakpoints: {
+          //   // when window width is >= 320px
+          //   320: {
+          //     slidesPerView: 1,
+          //     spaceBetween: 20
+          //   },
+          //   // when window width is >= 480px
+          //   480: {
+          //     slidesPerView: 1,
+          //     spaceBetween: 30
+          //   },
+          //   // when window width is >= 640px
+          //   640: {
+          //     slidesPerView: 1,
+          //     spaceBetween: 40
+          //   }
+          // },
+      });
 });
